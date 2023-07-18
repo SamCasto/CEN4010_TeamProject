faf865991a3aa10d78940079ac5f1db97b16874f
J. E. Cespedes <119894549+jences@users.noreply.github.com>
2023-07-18 00:34:09 -0400

    updated geektext/urls.py
    added routers for both ShoppingCartViewSet and CartItemViewSet

--- geektext/urls.py
+++ geektext/urls.py
@@ -8,6 +8,8 @@ router.register(r'publishers', views.PublisherViewSet)
 router.register(r'authors', views.AuthorViewSet)
 router.register(r'book', views.BookViewSet)
 router.register(r'users', views.UserViewSet)
+router.register(r'cart', views.ShoppingCartViewSet)
+router.register(r'cart/items', views.CartItemViewSet)
 
 
 urlpatterns = [
