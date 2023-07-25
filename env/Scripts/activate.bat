1ee422347d0827f55620986c086fbb5699013c0d
cebaBryan <bceballos2003@hotmail.com>
2023-07-24 20:37:31 -0400

    Update activate.bat

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\samue\Desktop\DjangoStuff\CEN4010_TeamProject\env
+set VIRTUAL_ENV=C:\Users\Bwyan\OneDrive\Desktop\CEN4010-Project\CEN4010_TeamProject\env
 
 if not defined PROMPT set PROMPT=$P$G
 
